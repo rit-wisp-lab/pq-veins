67619f2f4bebdcb9234c8d6633b0ba0de82e4161
Geoff Twardokus <gdt5762@rit.edu>
2023-09-30 08:48:48 +0000

    Add runfiles

---
+++ examples/veins/runs_2.sh
@@ -0,0 +1,8 @@
+./run -u Cmdenv -c V2V-Falcon-BPSK12-WITHP2PCD-500MS
+./run -u Cmdenv -c V2V-Falcon-BPSK12-WITHP2PCD-1000MS
+./run -u Cmdenv -c V2V-Falcon-BPSK12-WITHP2PCD-2000MS
+./run -u Cmdenv -c V2V-Falcon-BPSK12-WITHP2PCD-5000MS
+./run -u Cmdenv -c V2V-Falcon-BPSK34-WITHP2PCD-500MS
+./run -u Cmdenv -c V2V-Falcon-BPSK34-WITHP2PCD-1000MS
+./run -u Cmdenv -c V2V-Falcon-BPSK34-WITHP2PCD-2000MS
+./run -u Cmdenv -c V2V-Falcon-BPSK34-WITHP2PCD-5000MS
